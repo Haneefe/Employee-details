654e3fe234b3d5e4feb88075e4734181c5a0c329
Haneefe <haneefalikhan18@gmail.com>
2023-02-07 17:24:33 +0530

    Employees card created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import React, { useState } from "react";
-import "./App.css";
+import "./index.css";
 import Employee from "./components/Employee";
 function App() {
   const [role, setRole] = useState("dev");
+  const empData = [
+    {
+      name: "Anna",
+      role: "React Developer",
+      image:
+        "https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg",
+    },
+    {
+      name: "Emily",
+      role: "Dev",
+      image:
+        "https://images.pexels.com/photos/3201694/pexels-photo-3201694.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "Emma",
+      role: "Senior Dev",
+      image:
+        "https://images.pexels.com/photos/1181642/pexels-photo-1181642.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "Abby",
+      role: "Project Manager",
+      image:
+        "https://images.pexels.com/photos/4467687/pexels-photo-4467687.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "Casandra",
+      role: "Tester",
+      image:
+        "https://images.pexels.com/photos/4474047/pexels-photo-4474047.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+  ];
+  const employeeData = empData.map((emp, i) => {
+    return (
+      <Employee key={i} name={emp.name} image={emp.image} role={emp.role} />
+    );
+  });
   const ShowEmployees = true;
   const InputChangeHandler = (e) => {
     console.log(e.target.value);
@@ -11,12 +48,10 @@ function App() {
   return (
     <div className="App">
       {ShowEmployees ? (
-        <React.Fragment>
-          <input type="text" onChange={InputChangeHandler} />
-          <Employee name="Haneef" role="React Developer" />
-          <Employee name="Khan" role={role} />
-          <Employee name="David" role={role} />
-        </React.Fragment>
+        <div className="flex flex-wrap justify-center">
+          {/* <input type="text" onChange={InputChangeHandler} /> */}
+          {employeeData}
+        </div>
       ) : (
         <p>You cannot see Employees</p>
       )}
